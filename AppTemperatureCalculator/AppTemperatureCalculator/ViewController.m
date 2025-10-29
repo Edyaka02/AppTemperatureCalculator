8fa1ae5fbfbbcd8c39bceb0efafb41cff7cc5799
Edgar Manuel Carrillo Muruato <132968940+Edyaka02@users.noreply.github.com>
2025-10-28 17:07:33 -0700

    Add files via upload

--- AppTemperatureCalculator/AppTemperatureCalculator/ViewController.m
+++ AppTemperatureCalculator/AppTemperatureCalculator/ViewController.m
@@ -27,7 +27,7 @@
         self.outputLabel.text = @"0 Celsius";
     }
     
-    if(self.segControl.selectedSegmentIndex == 0){
+    if(self.segControl.selectedSegmentIndex == 1){
         // Convierte de Celsius a Fahrenheit
         self.enterLabel.text = @"Enter Celsius";
         self.textField.text = @"";
@@ -36,6 +36,55 @@
 }
 
 - (IBAction)convert:(id)sender {
-    
+    if(self.segControl.selectedSegmentIndex == 0){
+        double F = [self.textField.text doubleValue];
+        double C = (F - 32) / 1.8;
+        self.outputLabel.text = [NSString stringWithFormat:@"%4.2f Celsius", C];
+        
+        // Simular la temperatura en la imagen
+        if (C > 120) {
+            self.imageView.image = [UIImage imageNamed:@"Temp9"];
+        } else if (C > 100) {
+            self.imageView.image = [UIImage imageNamed:@"Temp8"];
+        } else if (C > 80) {
+            self.imageView.image = [UIImage imageNamed:@"Temp7"];
+        } else if (C > 60) {
+            self.imageView.image = [UIImage imageNamed:@"Temp6"];
+        } else if (C > 40) {
+            self.imageView.image = [UIImage imageNamed:@"Temp5"];
+        } else if (C > 20) {
+            self.imageView.image = [UIImage imageNamed:@"Temp4"];
+        } else if (C > 0) {
+            self.imageView.image = [UIImage imageNamed:@"Temp3"];
+        } else if (C > -10) {
+            self.imageView.image = [UIImage imageNamed:@"Temp2"];
+        } else if (C > -20) {
+            self.imageView.image = [UIImage imageNamed:@"Temp1"];
+        }
+    } else {
+        double C = [self.textField.text doubleValue];
+        double F = C * 1.8 + 32;
+        self.outputLabel.text = [NSString stringWithFormat:@"%4.2f Fahrenheit", F];
+        // Simular la temperatura en la imagen
+        if (F > 180) {
+            self.imageView.image = [UIImage imageNamed:@"Temp9"];
+        } else if (F > 160) {
+            self.imageView.image = [UIImage imageNamed:@"Temp8"];
+        } else if (F > 140) {
+            self.imageView.image = [UIImage imageNamed:@"Temp7"];
+        } else if (F > 120) {
+            self.imageView.image = [UIImage imageNamed:@"Temp6"];
+        } else if (F > 100) {
+            self.imageView.image = [UIImage imageNamed:@"Temp5"];
+        } else if (F > 80) {
+            self.imageView.image = [UIImage imageNamed:@"Temp4"];
+        } else if (F > 60) {
+            self.imageView.image = [UIImage imageNamed:@"Temp3"];
+        } else if (F > 40) {
+            self.imageView.image = [UIImage imageNamed:@"Temp2"];
+        } else if (F <= 40) {
+            self.imageView.image = [UIImage imageNamed:@"Temp1"];
+        }
+    }
 }
 @end
